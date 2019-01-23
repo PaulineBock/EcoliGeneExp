763ecaddeba1b82d154590c16a8ec14c49d5e4fc
GuillaumeSotton <33928461+GuillaumeSotton@users.noreply.github.com>
2019-01-23 15:04:37 +0100

    Update algoHierarchique.py

--- algoHierarchique.py
+++ algoHierarchique.py
@@ -72,9 +72,43 @@ def depthFirstSearch(tree, root, target, path):
     depthFirstSearch(tree, root, child)
    
   print path
+  
+def buildSmallMultiples(smallMultiplesTree,graph,timepoints):
+  for i in range(1,len(timepoints)+1):
+    tp=smallMultiplesTree.addSubGraph("tp"+str(i)+"_s")
+    inGraph=graph.getSubGraph("Genes interactions")
+    tlp.copyToGraph(tp,inGraph, inSelection=None, outSelection=None)
+  
+  property1=smallMultiplesTree.getDoubleProperty("viewMetric")
+  property2=smallMultiplesTree.getDoubleProperty("tp1 s")  
+  property1=property2
+  
     
 def main(graph): 
-  
+  tp1_s = graph.getDoubleProperty("tp1 s")
+  tp10_s = graph.getDoubleProperty("tp10 s")
+  tp11_s = graph.getDoubleProperty("tp11 s")
+  tp12_s = graph.getDoubleProperty("tp12 s")
+  tp13_s = graph.getDoubleProperty("tp13 s")
+  tp14_s = graph.getDoubleProperty("tp14 s")
+  tp15_s = graph.getDoubleProperty("tp15 s")
+  tp16_s = graph.getDoubleProperty("tp16 s")
+  tp17_s = graph.getDoubleProperty("tp17 s")
+  tp2_s = graph.getDoubleProperty("tp2 s")
+  tp3_s = graph.getDoubleProperty("tp3 s")
+  tp4_s = graph.getDoubleProperty("tp4 s")
+  tp5_s = graph.getDoubleProperty("tp5 s")
+  tp6_s = graph.getDoubleProperty("tp6 s")
+  tp7_s = graph.getDoubleProperty("tp7 s")
+  tp8_s = graph.getDoubleProperty("tp8 s")
+  tp9_s = graph.getDoubleProperty("tp9 s")
+  timepoints=[tp1_s,tp10_s,tp11_s,tp12_s,tp13_s,tp14_s,tp15_s,tp16_s,tp17_s,tp2_s,tp3_s,tp4_s,tp5_s,tp6_s,tp7_s,tp8_s,tp9_s]
+  size = graph.getSizeProperty("viewSize")
+  color = graph.getColorProperty("viewColor")
+  label = graph.getStringProperty("viewLabel")
+  shape = graph.getIntegerProperty("viewShape")
+  layout = graph.getLayoutProperty("viewLayout")
+  viewMetric = graph.getDoubleProperty("viewMetric")
   rootGraph=graph.getRoot()
   tree=rootGraph.addSubGraph(name='Hierarchical Tree')
   geneInteractions = rootGraph.getDescendantGraph('Genes interactions')
@@ -85,4 +119,8 @@ def main(graph):
   colorMapping(tree, tp1_s)
   print root
   bundleBuild(tree, geneInteractions, root)
+  
+  #Partie 3
+  smallMultiplesTree=rootGraph.addSubGraph(name='Small Multiples')
+  buildSmallMultiples(smallMultiplesTree,graph,timepoints)
 
